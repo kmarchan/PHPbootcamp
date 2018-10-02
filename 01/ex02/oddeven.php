87028da23a9e9ff35fed6a98815e231b138bca4f
Kmarchan <kmarchan@student.wethinkcode.co.za>
2018-10-02 16:24:59 +0200

    02

--- 01/ex02/oddeven.php
+++ 01/ex02/oddeven.php
@@ -1,18 +1,24 @@
 #!/usr/bin/php
 <?php
-
 echo "Enter a number: ";
-$in = trim(fgets(STDIN));
-while ($in = trim(fgets(STDIN))); 
+while ($in = fgets(STDIN)) 
 {
+    $in = trim($in);
     if (is_numeric($in))
     {
         if ($in % 2 == 0)
-            print "The number $in is even";
-        else if ($in %2 != 0)
-            print ("The number $in is odd");
+        {
+            echo "The number $in is even\n";
+        }
+        else
+        {
+           echo "The number $in is odd\n";
+        }
     }
-    else 
-        print ("'$in' is not a number");
+    else
+    {
+        echo "'$in' is not a number\n";
+    }
+    echo "Enter a number: ";
 }
 ?>
