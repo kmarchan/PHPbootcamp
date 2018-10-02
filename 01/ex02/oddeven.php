dddfbac3d4478d2882bcb745994027389379a668
Kirsten MARCHANT <kmarchan@c1r3s2.wethinkcode.co.za>
2018-10-02 10:18:25 +0200

    day 01

---
+++ 01/ex02/oddeven.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+
+echo "Enter a number: ";
+$in = trim(fgets(STDIN));
+while ($in = trim(fgets(STDIN))); 
+{
+    if (is_numeric($in))
+    {
+        if ($in % 2 == 0)
+            print "The number $in is even";
+        else if ($in %2 != 0)
+            print ("The number $in is odd");
+    }
+    else 
+        print ("'$in' is not a number");
+    // $in = trim(fgets(STDIN));
+}
+?>
