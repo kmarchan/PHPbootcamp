dd0a31d58f8ae8fb7eb8739200f9e1f0603ecec6
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-10-03 13:09:21 +0200

    02 01

--- 02/ex01/one_more_time.php
+++ 02/ex01/one_more_time.php
@@ -1,6 +1,17 @@
 #!/usr/bin/php
 <?php
     $str = $argv[1];
+    $frenchm = array(janvier, fevrier, mars, avril, mai, juin, juillet, aout, septembre, octobre, novembre, decembre);
+	$str = trim($str);
+	$noxspace = preg_replace('/\s\s+/', ' ', $str);
+    $ret = explode(' ', $noxspace);
+    $ret[2] = lcfirst("$ret[2]");
+    $key = array_search($ret[2], $frenchm);
+    $ret[2] = $key + 1;
     $in = time();
-    echo ($in. "<br>");
+    $in = $ret[3]."-".$ret[2]."-".$ret[1]." ".$ret[4];
+    $out = strtotime($in);
+    if ($out == FALSE)
+        echo "Wrong Format";
+    echo $out;
 ?>
