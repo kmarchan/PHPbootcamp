29f6aeec991887370b4183ec0841f3a7e43da864
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-10-04 09:12:10 +0200

    01 02

---
+++ 02/ex02/magnifying_glass.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?php
+   function str($file)
+   {
+       $i = -1;
+       while (++$i < strlen($file))
+       {
+           if($i = strpos($file, "<a", $i))
+           {
+               while ($file[$i] !== ">")
+                   $i++;
+               while ($file[++$i] !== "<")
+                   $file[$i] = strtoupper($file[$i]);
+           }
+           else
+               break ;
+       }
+       return ($file);
+   }
+   function title($file)
+   {
+       $i = -1;
+       while (++$i < strlen($file))
+       {
+           if ($i = strpos($file, "title=", $i))
+           {
+               while ($file[$i] !== "\"")
+                   $i++;
+               while ($file[++$i] !== "\"")
+                   $file[$i] = strtoupper($file[$i]);
+           }
+           else
+               break ;
+       }
+       return ($file);
+   }
+    if ($argv[1])
+    {
+        $file = file_get_contents($argv[1]);
+        $file = title($file);
+        $file = str($file);
+        echo $file;
+    }
+?>
