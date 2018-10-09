55e7ce20553afa6f6793fd3182d3ff0fcd6c8225
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-10-09 13:51:19 +0200

    upate and submit

--- 05/ex01/ex01.sql
+++ 05/ex01/ex01.sql
@@ -1,7 +1,7 @@
-CREATE TABLE bd_kmarchan.ft_table
+CREATE TABLE db_kmarchan.ft_table
 (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	login VARCHAR(8) NOT NULL DEFAULT 'toto',
-	grup ENUM('staff', 'student', 'other'),
+	grup ENUM('staff', 'student', 'other') NOT NULL,
 	creation_date DATE NOT NULL
 )
--- 05/ex03/ex03.sql
+++ 05/ex03/ex03.sql
@@ -1,4 +1,4 @@
-INSERT INTO db_kmarchan.ft_table (`login`, `creation_date`, `grup`)
+INSERT INTO ft_table (`login`, `creation_date`, `grup`)
 	SELECT last_name AS login, birthdate AS creation_date, 'other' AS grup
 	FROM db_kmarchan.user_card
 	WHERE last_name LIKE '%a%' AND LENGTH(last_name) < 9
