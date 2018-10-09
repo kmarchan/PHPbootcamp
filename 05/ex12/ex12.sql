dac887530e058b329cf8f9094ce73e152ff5ae18
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-10-09 13:31:42 +0200

    last edits

--- 05/ex12/ex12.sql
+++ 05/ex12/ex12.sql
@@ -1,5 +1,5 @@
 SELECT last_name, first_name 
     FROM `db_kmarchan`.`user_card`
-    INNER JOIN db_kmarchan.member ON user_card.id_user=member.member_id 
+    INNER JOIN db_kmarchan.member ON user_card.id_user=member.id_member 
     WHERE `last_name` LIKE '%-%' OR `first_name` LIKE '%-%' 
     ORDER BY `last_name` ASC, `first_name`; 
